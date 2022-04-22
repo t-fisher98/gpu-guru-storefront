94a6c6e095b26ce24031a10743396b75772c83ff
Tyler Fisher <tyler.fisher98@gmail.com>
2022-04-22 14:07:36 -0600

    Adds product render functionality

---
+++ components/products/ProductCard/ProductCard.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import Card from 'components/card/Card'
+import { 
+	Box,
+	Text,
+	Flex,
+	Button
+} from '@chakra-ui/react'
+import Image from 'next/image'
+
+function ProductCard({product}) {
+
+	const {productName, productPrice, imageUrl, productDescription, uid} = {...product};
+
+	return (
+        <Card height="35rem" position="relative">
+            <Box borderBottom="1px solid lightgrey" padding="1rem">
+                <Flex justifyContent="end">
+                    <Text fontSize="2rem" color="grey">
+                        ${productPrice}
+                    </Text>
+                </Flex>
+                <Image src={imageUrl} alt={productName.value} width='480' height='500'/>
+            </Box>
+            <Flex flexDirection="column" padding="1rem">
+                <Text fontSize="1.5rem">{productName}</Text>
+                <Text lineHeight="1.25">{productDescription}</Text>
+            </Flex>
+            <Flex position="absolute" right="1rem" bottom="1rem">
+                <form>
+                    <input type="hidden" name="uid" value={uid} />
+                    <Button
+                        type="submit"
+                        bgColor="gray.600"
+                        color="white"
+                        _hover={{ bg: "blue.500" }}
+                    >
+                        Buy Now
+                    </Button>
+                </form>
+            </Flex>
+        </Card>
+    );
+}
+
+export default ProductCard
